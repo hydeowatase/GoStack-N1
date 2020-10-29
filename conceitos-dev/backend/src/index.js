c75e07bf4639ffd0bd92bf7995b158c718e403ca
hydeowatase <hydeowatase@gmail.com>
2020-10-28 22:31:16 -0300

    Created a POC of API to CRUD projects on a Array of objects.

---
+++ conceitos-dev/backend/src/index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const { uuid } = require('uuidv4');
+
+app.use(express.json());
+
+//Only for dev
+const projects = [];
+
+app.get("/projects", (request, response) => {
+    const { title } = request.query;
+
+    const results = title 
+    ? projects.filter(project => project.title.includes(title)) 
+    : projects;
+
+    return response.json(results);
+});
+
+app.post("/projects", (request, response) => {
+    const { title, owner } = request.body;
+
+    const project = { id: uuid(), title, owner }
+
+    projects.push(project);
+
+    return response.json(project);
+});
+
+app.put("/projects/:id", (request, response) => {
+    const { id } = request.params;
+    const { title, owner } = request.body;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: "Project not found." });
+    }
+
+    const project = { id, title, owner }
+
+    projects[projectIndex] = project;
+
+    return response.json(project);
+});
+
+app.delete("/projects/:id", (request, response) => {
+    const { id } = request.params;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: "Project not found." });
+    }
+
+    projects.splice(projectIndex, 1)
+
+    return response.status(204).send();
+
+});
+
+
+app.listen(3333, () => {
+    console.log("Back-end started!🚀");
+});
